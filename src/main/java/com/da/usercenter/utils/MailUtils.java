c249a3f71b2ac2d02299697236d26386a3220731
Da <1349190697@qq.com>
2023-10-14 20:41:57 +0800

    ~~

---
+++ src/main/java/com/da/usercenter/utils/MailUtils.java
@@ -0,0 +1,18 @@
+package com.da.usercenter.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import javax.annotation.Resource;
+import java.util.Random;
+
+/**
+ * 邮件工具类
+ */
+public class MailUtils {
+    
+    
+
+
+}
